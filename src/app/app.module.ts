50996ede451d0469ca790ec9b2ee25ffe3afd55a
Hugo Medeiros <hugo.hugomedeiros@gmail.com>
2017-11-24 01:06:20 -0200

    Customize LogIn, new Page,

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AppComponent } from './app.component';
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
+import { SettingsPage } from '../pages/settings/settings';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -44,6 +45,7 @@ export class SkipAppErrorHandler implements ErrorHandler {
     AboutPage,
     ContactPage,
     HomePage,
+		SettingsPage,
     TabsPage
   ],
   imports: [
@@ -56,6 +58,7 @@ export class SkipAppErrorHandler implements ErrorHandler {
     AboutPage,
     ContactPage,
     HomePage,
+		SettingsPage,
     TabsPage
   ],
   providers: [
