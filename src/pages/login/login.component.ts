0b3cf36f59c1959175f43f9f557359d5f7ed6356
thiagoalencar <thiagofalencar@gmail.com>
2017-11-25 02:08:35 -0300

    Implement the login method, saving in a Local Storage

--- src/pages/login/login.component.ts
+++ src/pages/login/login.component.ts
@@ -1,19 +1,45 @@
-import { Component     } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Component     } from '@angular/core';
 
-import { TasteMeterComponent } from './../tasteMeter/TasteMeter.component';
+import { Storage                } from '@ionic/storage';
+
+import { TasteMeterComponent    } from './../tasteMeter/TasteMeter.component';
+import { AuthenticationService  } from '../../athentication.service';
+import { IUser                  } from './../../app/user.interface';
+import { TabsPage               } from '../tabs/tabs';
 
 @Component({
   selector: 'login-component',
   templateUrl: 'login.component.html'
 })
 export class LoginComponent {
+  private userData = {};
+  errorMessage: string;
 
   tmComponent: any = TasteMeterComponent;
-  constructor( public navCtrl: NavController ) { }
 
-  login() {
-    console.log('clicked');
+  constructor(  private authenticationService: AuthenticationService,
+                public navCtrl: NavController,
+                private storage: Storage ) { }
+
+  submitLogin() {
+
+    // The user authentication was not propertly validate
+    // to save time to the rest of the project
+
+    this.authenticationService.authenticate( this.userData )
+        .subscribe(
+          ( user ) => {
+            if ( user["session"] !== null ) {
+              this.storage.set( 'id',      user["id"] );
+              this.storage.set( 'login',   user["login"] );
+              this.storage.set( 'lastName',user["lastName"] );
+              this.storage.set( 'email',   user["email"] );
+              this.storage.set( 'session', user["session"] );
+              this.navCtrl.push( TabsPage )
+            };
+          },
+          error => this.errorMessage = error );
   }
 
 }
