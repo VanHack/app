f005c5a609a7a1392bc1c712a88b411c0e0e39a5
Hugo Medeiros <hugo.hugomedeiros@gmail.com>
2017-11-26 08:26:43 -0200

    Fix responsive, remove hardcoded values

--- src/pages/tasteMeter-Suggestions/TasteMeter-Suggestions.component.ts
+++ src/pages/tasteMeter-Suggestions/TasteMeter-Suggestions.component.ts
@@ -18,10 +18,10 @@ export class TasteMeterSuggestionsComponent implements OnInit {
   constructor( public navCtrl: NavController ) { }
 
   slides = [
-    {img: "https://source.unsplash.com/750x900/?salad", name:"Green Salad"},
-    {img: "https://source.unsplash.com/750x900/?burger", name: "Monster BBQ Burger"},
-    {img: "https://source.unsplash.com/750x900/?pasta", name: "Fettuccini Alfredo"},
-    {img: "https://source.unsplash.com/750x900/?chinese,food", name:"Teriaky Chiken"}
+    {img: "https://source.unsplash.com/750x900/?salad", name:"Green Salad", venue:"The Vegan Squad", price: "14.90"},
+    {img: "https://source.unsplash.com/750x900/?burger", name: "Monster BBQ Burger", venue:"Tipsy Cow", price:"23.50"},
+    {img: "https://source.unsplash.com/750x900/?pasta", name: "Fettuccine Alfredo", venue:"Olive Garden", price: "36.70"},
+    {img: "https://source.unsplash.com/750x900/?chinese,food", name:"Teriaky Chiken", venue:"Panda Chinese Restaurant", price: "8.90"}
   ];
 
   slideConfig = {
@@ -37,7 +37,7 @@ export class TasteMeterSuggestionsComponent implements OnInit {
   addSlide() {
     console.log( 'Slide added' );
     this.slides.push(
-      { img: "https://source.unsplash.com/750x900/?dessert,strawberry,pie", name: "Strawberry Pie" }
+      { img: "https://source.unsplash.com/750x900/?dessert,strawberry,pie", name: "Strawberry Pie", venue: "Baked Expectations", price:"15.90" }
     )
   }
 
